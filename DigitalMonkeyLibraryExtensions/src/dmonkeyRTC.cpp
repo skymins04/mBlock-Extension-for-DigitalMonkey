4ca340c02bc76a51c8a4ef426c7b6071f01f37b6
Minsoo Kang (BetaMan) <33021438+skymins04@users.noreply.github.com>
2018-02-03 17:45:28 +0900

    180203 지원 모듈 추가, 블럭 가독성 향상

---
+++ DigitalMonkeyLibraryExtensions/src/dmonkeyRTC.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "dmonkeyRTC.h"
+
+#include "DS1307RTC.h"
+#include "TimeLib.h"
+
+boolean dmonkeyRTC::getTime(const char *str) {
+	int Hour, Min, Sec; 
+	if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false; 
+	tm.Hour = Hour; 
+	tm.Minute = Min; 
+	tm.Second = Sec; 
+	return true;
+}
+
+boolean dmonkeyRTC::getDate(const char *str) {
+	char Month[12]; 
+	int Day, Year; uint8_t monthIndex; 
+	if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false; 
+	for (monthIndex = 0; monthIndex < 12; monthIndex++) { 
+		if (strcmp(Month, monthName[monthIndex]) == 0) break; 
+	} 
+	if (monthIndex >= 12) return false; 
+	tm.Day = Day; 
+	tm.Month = monthIndex + 1; 
+	tm.Year = CalendarYrToTm(Year); 
+	return true; 
+}
+
+void dmonkeyRTC::setRTC() {
+	if(getDate(__DATE__)&&getTime(__TIME__)) RTC.write(tm);
+}
+
+String dmonkeyRTC::readTime() {
+	RTC.read(tm);
+	String Time = String(tm.Hour) + String(":") + String(tm.Minute) + String(":") + String(tm.Second);
+	return Time; 
+}
+
+String dmonkeyRTC::readDate() {
+	RTC.read(tm);
+	String Date = tmYearToCalendar(tm.Year) + String("-") + tm.Month + String("-") + tm.Day;
+	return Date;
+}
---
+++ DigitalMonkeyLibraryExtensions/src/dmonkeyRTC.h
@@ -0,0 +1,18 @@
+#ifndef dmonkeyRTC_H
+#define dmonkeyRTC_H
+
+#include "DS1307RTC.h"
+
+class dmonkeyRTC {
+	private:
+		tmElements_t tm;
+		const char *monthName[12] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+		boolean getTime(const char *str);
+		boolean getDate(const char *str);
+	public:
+		void setRTC();
+		String readTime();
+		String readDate();
+};
+
+#endif
